53b642c099530732e31e11473faff303daada8d7
DaiNghia0212 <dainghia12311@gmail.com>
2023-02-21 20:47:15 +0700

    Initial commit

---
+++ src/java/models/order_detail/OrderDetailFacade.java
@@ -0,0 +1,15 @@
+package models.order_detail;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author daing
+ */
+public class OrderDetailFacade {
+    
+}
