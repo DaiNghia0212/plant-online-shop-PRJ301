de7341b78931f19f5267185c4004239f9d002529
DaiNghia0212 <dainghia12311@gmail.com>
2023-02-23 14:11:42 +0700

    handle login action

---
+++ src/java/controllers/AuthenticationController.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author daing
+ */
+@WebServlet(name = "Authentication", urlPatterns = {"/auth"})
+public class AuthenticationController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+//            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet AuthenticationController</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet AuthenticationController at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+//    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+        String action = (String) request.getAttribute("action");
+        switch (action) {
+            case "login": {
+                request.getRequestDispatcher("/WEB-INF/pages/authentication/login.jsp").forward(request, response);
+//                response.sendRedirect("/WEB-INF/pages/authentication/login.jsp");
+            }
+            default:
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        processRequest(request, response);
+        String action = (String) request.getAttribute("action");
+        switch (action) {
+            case "login": {
+                String inputEmail = request.getParameter("email");
+                String inputPassword = request.getParameter("password");
+//        String password = get password from database
+                if (inputPassword.equals("123")) {
+                    response.sendRedirect(request.getContextPath() + "/home/index.do");
+                } else {
+                    request.setAttribute("message", "Wrong email or password");
+                    request.getRequestDispatcher("/WEB-INF/pages/authentication/login.jsp").forward(request, response);
+                }
+            }
+            default:
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    } // </editor-fold>
+
+}
---
+++ web/WEB-INF/pages/authentication/login.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : login
+    Created on : Feb 23, 2023, 8:46:30 AM
+    Author     : daing
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Plant Shop Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        <form name="input" action="login.do" method="post">
+            <table>
+                <tr>
+                    <td>email:</td><td><input type="text" name="email" required/></td>
+                </tr>
+                <tr>
+                    <td>Password:</td><td><input type="password" name="password" required/></td>
+                </tr>
+                <tr>
+                    <td></td><td><input type="checkbox" name="rememberMe"/>Remember Me</td>
+                </tr>
+                <tr>
+                    <td></td><td><button type="submit">Login</button></td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
