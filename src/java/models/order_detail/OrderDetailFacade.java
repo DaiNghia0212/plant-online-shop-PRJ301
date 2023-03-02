040acd7efac1f266ea0a3cddb99fab3f1304177e
Nguyen Van~ <vannam129001@gmail.com>
2023-03-02 14:21:30 +0700

    orderDetailFacade

--- src/java/models/order_detail/OrderDetailFacade.java
+++ src/java/models/order_detail/OrderDetailFacade.java
@@ -1,5 +1,11 @@
 package models.order_detail;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import models.DBContext;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,5 +17,25 @@ package models.order_detail;
  * @author daing
  */
 public class OrderDetailFacade {
-    
+        public static ArrayList<OrderDetail> getOrderDetail(int orderId) throws Exception {
+        ArrayList<OrderDetail> list = new ArrayList<>();
+        Connection cn = DBContext.getConnection();
+        if (cn != null) {
+            String sql = "select order_id,product_id,quantity\n"
+                    + "from order_details\n"
+                    + "where order_id=? ";
+            PreparedStatement pst = cn.prepareStatement(sql);
+            pst.setInt(1, orderId);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+               
+                int productId = rs.getInt("product_id");
+                int quantity = rs.getInt("quantity");
+                OrderDetail orderDetail = new OrderDetail(orderId, productId, quantity);
+                list.add(orderDetail);
+            }
+            cn.close();
+        }
+        return list;
+    }
 }
